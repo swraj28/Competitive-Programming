e576a720599eec8ad3c55680bceeaa44656badc1
swraj28 <swrajkant28@gmail.com>
2021-10-06 15:39:37 +0530

    bipartite bfs

--- Graph/Undirected Graph/Bipartite.cpp
+++ Graph/Undirected Graph/Bipartite.cpp
@@ -42,6 +42,49 @@ bool dfs(vector<int> graph[], int n) {
 	return ans;
 }
 
+// Bipartiteness using BFS:-
+
+bool bipartiteBfs(int src, vector<int> adj[], int color[]) {
+
+	queue<int>q;
+	q.push(src);
+	color[src] = 1;
+
+	while (!q.empty()) {
+
+		int node = q.front();
+		q.pop();
+
+		for (auto it : adj[node]) {
+
+			if (color[it] == -1) {
+				color[it] = 1 - color[node];
+				q.push(it);
+			} else if (color[it] == color[node]) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool checkBipartite(vector<int> adj[], int n) {
+
+	int color[n];
+	memset(color, -1, sizeof color);
+
+	for (int i = 0; i < n; i++) {
+
+		if (color[i] == -1) {
+			if (!bipartiteBfs(i, adj, color)) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
