14080400dcc5d15a04883130b568e4c11272e4ca
swraj28 <swarajkant@gmail.com>
2022-03-03 09:04:57 +0530

    03-03-2022

--- Dp/5.CN_Top_100_DP/Longest_Alternating_Subsequence.cpp
+++ Dp/5.CN_Top_100_DP/Longest_Alternating_Subsequence.cpp
@@ -12,32 +12,36 @@ using namespace std;
 #define all(v) (v).begin(),(v).end()
 #define ms(arr, v) memset(arr, v, sizeof(arr))
 
+// T.c:- O(n^2) && S.c:- O(n*3)=O(n)  ---> TLE (Gfg)
+
+// Similar Question --> Weegle Subsequence (Leetcode)--> Constraints are small. Therefore it will pass the O(n^2) Approach.
+
+int dp[100005][2];
+
 int recur(vector<int> &arr, int n, int si, int prev) {
-	if (si >= n - 1) {
+
+	if (si >= n) {
 		return 0;
+	}
+
+	if (dp[si][prev] != -1) {
+		return dp[si][prev];
 	}
 
 	int mx = 0;
 
-	for (int i = si + 1; i < n; i++) {
-		if (prev == 0) {
-			if (arr[i] > arr[si]) {
-				mx = max({mx, 2 + recur(arr, n, i, 1)});
-			} else if (arr[i] < arr[si]) {
-				mx = max({mx, 2 + recur(arr, n, i, 2)});
-			}
-		} else if (prev == 1) {
-			if (arr[i] < arr[si]) {
-				mx = max({mx, 1 + recur(arr, n, i, 2)});
-			}
-		}
+	for (int i = si; i < n; i++) {
 
-		if (arr[i] > arr[si]) {
-			mx = max({mx, 1 + recur(arr, n, i, 1)});
+		if ((arr[i] > arr[i - 1]) && prev == 1) {
+			mx = max({mx, 1 + recur(arr, n, i + 1, 0)});
+		} else if ((arr[i] < arr[i - 1]) && prev == 0) {
+			mx = max({mx, 1 + recur(arr, n, i + 1, 1)});
+		} else {
+			mx = max({mx, recur(arr, n, i + 1, prev)});
 		}
 	}
 
-	return mx;
+	return dp[si][prev] = mx;
 }
 
 int AlternatingaMaxLength(vector<int>&nums) {
@@ -45,10 +49,20 @@ int AlternatingaMaxLength(vector<int>&nums) {
 	int n = nums.size();
 
 	if (n <= 1) {
-		return 0;
+		return 1;
 	}
 
-	int ans = recur(nums, n, 0, 0);
+	ms(dp, -1);
+
+	int ans = 1 + max({recur(nums, n, 1, 1), recur(nums, n, 1, 0)});
 
 	return ans;
 }
+
+int lSusbsequence(vector<int> &arr, int n) {
+
+	return AlternatingaMaxLength(arr);
+}
+
+// Optimization:-  The Only thing that is required is to  count the increasing and decreasing peaks.
+
